ffdfc0b29981702ddec9617327fcdcb75f5cc74e
Ka Wai Wong <kw.wong0923@gmail.com>
2023-05-08 01:10:25 -0400

    updated React API URL

--- client/src/services/course.service.js
+++ client/src/services/course.service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:8080/api/course";
+const API_URL = "https://onlinecoursemernproject.herokuapp.com/api/course";
 
 class CourseService
 {
